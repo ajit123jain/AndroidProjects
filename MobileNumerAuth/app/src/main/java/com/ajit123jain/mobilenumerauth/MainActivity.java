0d2a6e27f419693cc1ba7095851fbdf4b5eee941
ajit-999 <ajitkumarjain123@gmail.com>
2017-10-19 16:09:24 +0530

    Number format

--- MobileNumerAuth/app/src/main/java/com/ajit123jain/mobilenumerauth/MainActivity.java
+++ MobileNumerAuth/app/src/main/java/com/ajit123jain/mobilenumerauth/MainActivity.java
@@ -1,20 +1,31 @@
 package com.ajit123jain.mobilenumerauth;
 
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    Button mLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+        mLogout = (Button)findViewById(R.id.logout);
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activityChange();
+            }
+        });
       
     }
 
@@ -31,4 +42,11 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
     }
+     private void activityChange(){
+         mAuth.signOut();
+         Intent authIntent = new Intent(MainActivity.this,AuthActivity.class);
+         startActivity(authIntent);
+         finish();
+     }
+
 }
