7c2fffa2051ea50a912b814498976113c413d710
ajit-999 <ajitkumarjain123@gmail.com>
2017-10-28 19:10:34 +0530

    Cient and Server Added

---
+++ Server/app/src/main/java/rajeevpc/server/MainActivity.java
@@ -0,0 +1,26 @@
+package rajeevpc.server;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Server server;
+    TextView infoip, msg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        infoip = (TextView) findViewById(R.id.infoip);
+        msg = (TextView) findViewById(R.id.msg);
+        server = new Server(this);
+        infoip.setText(server.getIpAddress() + ":" + server.getPort());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        server.onDestroy();
+    }
+}
